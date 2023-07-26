347fca1d710b171e32b2eb3333d624320d012474
LAZY-2001 <140515880+LAZY-2001@users.noreply.github.com>
2023-07-26 16:35:42 +0800

    Delete MDK-ARM directory

--- MDK-ARM/Control/bullet_model.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BULLET_MODEL_H
-#define __BULLET_MODEL_H
-
-#include "stm32f4xx_hal.h"
-
-float BulletModelGetPitch(float dis, float height, float ammo_speed);
-
-#endif /* __BULLET_MODEL_H */
